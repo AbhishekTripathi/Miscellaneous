98d32099e5f97670a4815b236a6b7bf6cdac1089
Abhishek Tripathi <abhishekt@angieslist.com>
2016-02-10 17:04:05 +0530

    Added input file snippet

--- sysinternals_info.fsx
+++ sysinternals_info.fsx
@@ -7,8 +7,21 @@ open System.Text.RegularExpressions
 open Microsoft.FSharp.Data
 open Microsoft.FSharp.Linq
 
-//#time
+//Sample File Content Begins Below
+//Sysinternals Utilities Index
+//https://technet.microsoft.com/en-us/sysinternals/bb545027
+//Sysinternals Suite
+//The entire set of Sysinternals Utilities rolled up into a single download.
+//==========================================================================
+//AccessChk
+//v6.01 (January 4, 2016)
+//AccessChk is a command-line tool for viewing the effective permissions on files, registry keys, services, processes, kernel objects, and more.
+//AccessEnum
+//v1.32 (November 1, 2006)
+//This simple yet powerful security tool shows you who has what access to directories, files and Registry keys on your systems. Use it to find holes in your permissions.
+//AdExplorer
 
+//#time
 let fileContent = File.ReadAllLines(@"d:\dropbox\Tools\sysinternals\sysinternals_list.txt").Skip(5).ToArray()
 type AppInfo = {Name:string; Version: string; ReleaseDate: string; Description: string}
 let regexVersion = @"^(\S+)"
